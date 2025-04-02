9aea2c19048838245a8f280d42fde346874d3796
Mario Debono <mario.debono@gmail.com>
2025-04-02 23:57:31 +0200

    feat: enable trailing slash in Docusaurus configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -27,6 +27,7 @@ const config: Config = {
   onBrokenMarkdownLinks: "throw",
 
   baseUrlIssueBanner: false,
+  trailingSlash: true,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
